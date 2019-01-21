3839fe35384929e82580bbf236e2551b1800a652
GongJintao <1154509267@qq.com>
2019-01-21 14:07:21 +0800

    第一天打卡

--- src/main.js
+++ src/main.js
@@ -1,17 +1,34 @@
 //入口文件
 import Vue from 'vue'
 
+//1.1导入路由的包
+import VueRouter from 'vue-router'
+//1.2 全局注册路由组件
+Vue.use(VueRouter)
+//1.3 导入自己的 router.js 模块
+import router from  './router.js'
+
+
+//2.1 导入 vue-resource
+import VueResource from 'vue-resource'
+//全局使用vue-resource
+Vue.use(VueResource)
+
 //按需导入Mint-UI中的组件
-import { Header} from 'mint-ui'
+import { Header,Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 //导入mui的样式
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 //导入 App 根组件
 import app from  './App.vue'
 
 var vm = new Vue({
     el:'#app',
-    render: c=>c(app)
+    render: c=>c(app),
+    router,//1.4挂载路由对象到vm实例上
 })
--- src/router.js
+++ src/router.js
@@ -1,10 +1,20 @@
 import VueRouter from  'vue-router'
 
+//导入对应的路由组件
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
 
 var router = new VueRouter({
     routes:[
- 
-    ]
+        {path:'/',redirect:'/home'},
+        {path:'/home',component:HomeContainer},
+        {path:'/member',component:MemberContainer},
+        {path:'/cart',component:ShopcarContainer},
+        {path:'/search',component:SearchContainer},
+    ],
+    linkActiveClass:'mui-active',//覆盖默认路由高亮的类：router-link-active
 })
 
 export default router 
